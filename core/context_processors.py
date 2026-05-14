029a21582522add268cba9695815deb07ceb1163
irfan564 <muhammedirfanma112@gmail.com>
2026-05-14 22:12:41 +0530

    number changed

--- core/context_processors.py
+++ core/context_processors.py
@@ -6,7 +6,7 @@ def global_context(request):
         'whatsapp_number': '918075176518',
         'whatsapp_message': 'Hello Stainy Henna! I would like to know more about your services.',
         'business_email': 'hello@stainyhenna.com',
-        'business_phone': '+91 80751 76518',
+        'business_phone': '+91 8086613259',
         'business_address': 'Kochi, Kerala, India',
         'instagram_url': 'https://www.instagram.com/stainy._hennah?igsh=MWIzZ291cnk1a250cw==',
         'facebook_url': 'https://facebook.com/stainyhenna',
